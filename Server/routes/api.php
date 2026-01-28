cd8dcb7f5b25f4ded7d1176bfded28aeff92af98
Safwan <safwanpaloli6@gmail.com>
2026-01-28 22:02:15 +0530

    chore(api): install API scaffolding and Sanctum configuration

---
+++ Server/routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/tasks', [TaskController::class, 'index']);
+    Route::post('/tasks', [TaskController::class, 'store']);
+});
